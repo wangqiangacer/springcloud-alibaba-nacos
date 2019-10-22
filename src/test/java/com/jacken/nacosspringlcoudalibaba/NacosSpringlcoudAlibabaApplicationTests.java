05a50a53550bfd69bf00905251a77aa17daa19c4
wangqiang <wqexpore@163.com>
2019-10-22 17:36:03 +0800

    springcloud-alibaba 使用nacos作为注册中心

---
+++ src/test/java/com/jacken/nacosspringlcoudalibaba/NacosSpringlcoudAlibabaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jacken.nacosspringlcoudalibaba;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSpringlcoudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
